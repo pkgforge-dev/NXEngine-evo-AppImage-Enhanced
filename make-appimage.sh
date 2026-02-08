3ec8898af4cca542caac0ba546962c0a23952d22
Link4Electronics <link4electronics@gmail.com>
2026-02-08 18:47:33 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,8 @@
 set -eu
 
 ARCH=$(uname -m)
-#VERSION=$(pacman -Q nxengine-evo | awk '{print $2; exit}') # example command to get version of application here
-export ARCH
+VERSION=$(pacman -Q nxengine-evo-git | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -14,7 +14,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/nxengine-evo
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 

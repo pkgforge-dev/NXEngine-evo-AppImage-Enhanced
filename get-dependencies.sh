83610199c1960d414de5c482bee463b070be5229
Link4Electronics <link4electronics@gmail.com>
2026-02-08 18:49:40 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,43 +7,43 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor
-    #cmake      \
-    #sdl2_image \
-    #sdl2_mixer \
-    #sdl2_ttf
+    cmake      \
+    libdecor   \
+    sdl2_image \
+    sdl2_mixer \
+    sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
-#echo "Making nightly build of NXEngine-evo..."
-#echo "---------------------------------------------------------------"
-#REPO="https://github.com/nxengine/nxengine-evo"
-#VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-#git clone "$REPO" ./NXEngine-evo
-#echo "$VERSION" > ~/version
+echo "Making nightly build of NXEngine-evo..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/nxengine/nxengine-evo"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./NXEngine-evo
+echo "$VERSION" > ~/version
 
-#mkdir -p ./AppDir/bin
-#cd ./NXEngine-evo
-#wget https://www.cavestory.org/downloads/cavestoryen.zip
-#wget https://github.com/nxengine/translations/releases/download/v1.14/all.zip
-#bsdtar -xvf ./cavestoryen.zip
-#bsdtar -xvf ./all.zip
-#mkdir -p build && cd build
-#cmake .. \
-#    -DCMAKE_BUILD_TYPE=Release \
-#    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-#    -DCMAKE_INSTALL_PREFIX="/usr"
-#make -j$(nproc)
-#cp -r "../CaveStory/data/" "./"
-#cp "../CaveStory/Doukutsu.exe" "./"
-#cp -r "../data/" "./"
-#./nxextract
-#make install
-#cd ..
-#mv -v /usr/share/nxengine ../AppDir/bin
+mkdir -p ./AppDir/bin
+cd ./NXEngine-evo
+wget https://www.cavestory.org/downloads/cavestoryen.zip
+wget https://github.com/nxengine/translations/releases/download/v1.14/all.zip
+bsdtar -xvf ./cavestoryen.zip
+bsdtar -xvf ./all.zip
+mkdir -p build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_INSTALL_PREFIX="/usr"
+make -j$(nproc)
+cp -r "../CaveStory/data/" "./"
+cp "../CaveStory/Doukutsu.exe" "./"
+cp -r "../data/" "./"
+./nxextract
+make install
+cd ..
+mv -v /usr/share/nxengine ../AppDir/bin

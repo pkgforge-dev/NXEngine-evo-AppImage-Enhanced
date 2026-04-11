0153e3484536be2baa6d2a4112093aa7b5616ccc
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:34:43 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,21 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q nxengine-evo-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export APPNAME="Cave Story NX-evo"
-export ICON=/usr/share/icons/hicolor/256x256/apps/org.nxengine.nxengine_evo.png
-export DESKTOP=/usr/share/applications/org.nxengine.nxengine_evo.desktop
+export ICON=https://raw.githubusercontent.com/nxengine/nxengine-evo/refs/heads/master/platform/xdg/org.nxengine.nxengine_evo.png
+export DESKTOP=https://raw.githubusercontent.com/nxengine/nxengine-evo/refs/heads/master/platform/xdg/org.nxengine.nxengine_evo.desktop
 export STARTUPWMCLASS=nxengine-evo
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/nxengine-evo
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/nxengine-evo
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage

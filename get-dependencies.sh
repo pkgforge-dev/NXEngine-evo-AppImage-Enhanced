6b49f8b4775687302da20133433bf7956be4c3b4
Link4Electronics <link4electronics@gmail.com>
2026-07-01 08:47:09 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,30 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor
+pacman -Syu --noconfirm cmake sdl2_mixer sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
-make-aur-package
+echo "Building NXEngine-evo..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/nxengine/nxengine-evo"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./NXEngine-evo
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cd ./NXEngine-evo
+wget https://www.cavestory.org/downloads/cavestoryen.zip
+wget https://github.com/nxengine/translations/releases/download/v1.14/all.zip
+bsdtar -xvf ./cavestoryen.zip
+bsdtar -xvf ./all.zip
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DPORTABLE=ON
+make -j$(nproc)
+cp -r ../CaveStory/data/ ./
+cp ../CaveStory/Doukutsu.exe ./
+cp -r ../data/ ./
+./nxextract
+mv -v nxengine-evo data ../../AppDir/bin

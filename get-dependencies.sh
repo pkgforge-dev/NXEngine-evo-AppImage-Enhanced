f5953c07366a2e97afb89a132a9fbdbcb40127dc
Link4Electronics <link4electronics@gmail.com>
2026-08-31 10:51:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,10 +15,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
 echo "Building NXEngine-evo..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/nxengine/nxengine-evo"

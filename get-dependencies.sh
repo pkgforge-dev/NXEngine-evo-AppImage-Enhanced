2527dd680e8de1b55c718684c51d3fedf4b1944c
Link4Electronics <link4electronics@gmail.com>
2026-09-08 23:47:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,10 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake      \
-    sdl2_mixer \
-    sdl2_ttf
+pacman -Syu --noconfirm cmake sdl2_mixer sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
